dff40058b54b2d3210fb88d0bee2bf1131c17cd5
shane.chi <yuhsuan19@gmail.com>
2023-12-28 18:42:36 +0800

    feat: Utility

---
+++ test/Utility.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+abstract contract Utility {
+    address constant _uniswapV2Factory = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
+    address constant _uniswapV2Router = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+
+    address constant _chainlink = 0x514910771AF9Ca656af840dff83E8264EcF986CA;
+    address constant _usdt = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
+    address constant _usdc = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address constant _uni = 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984;
+    address constant _pepe = 0x6982508145454Ce325dDbE47a25d4ec3d2311933;
+    address constant _comp = 0xc00e94Cb662C3520282E6f5717214004A7f26888;
+    address constant _wbtc = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
+}
